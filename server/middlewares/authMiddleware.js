b2ad8cf279035a046eb8b3fd65205ec33128cdcc
Georgi Markov <gogata1905@gmail.com>
2024-10-20 00:18:33 +0300

    add auth middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,39 @@
+// server\middlewares\authMiddleware.js
+const { TOKEN_COOKIE_NAME, SECRET } = require("../config/constants");
+const jwt = require("jsonwebtoken");
+
+exports.auth = function (req, res, next) {
+  let token = req.cookies[TOKEN_COOKIE_NAME];
+
+  if (!token) {
+    return next();
+  }
+
+  jwt.verify(token, SECRET, function (err, decodedToken) {
+    if (err) {
+      console.log("Invalid token, clearing cookie.");
+      res.clearCookie(TOKEN_COOKIE_NAME, { path: "/" });
+      return next();
+    }
+
+    req.user = decodedToken;
+
+    next();
+  });
+};
+
+exports.isAuth = function (req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ error: "You are not logged in!" });
+  }
+
+  next();
+};
+
+exports.isAlreadyLogged = function (req, res, next) {
+  if (req.user) {
+    return res.status(401).json({ error: "You are already logged in!" });
+  }
+
+  next();
+};
