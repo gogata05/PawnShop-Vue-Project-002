33afd6f9e45fc67f2eb898e9c702646a81db4b9c
Georgi Markov <gogata1905@gmail.com>
2024-10-21 19:22:34 +0300

    add top10,favorite and edit functions in productService

--- server/services/productServices.js
+++ server/services/productServices.js
@@ -4,6 +4,12 @@ const User = require("../models/User");
 
 const create = data => Product.create(data);
 const getAll = () => Product.find({});
+const getTop10 = () =>
+  Product.find({})
+    .sort([["rating", "desc"]])
+    .limit(10);
+const getOne = id => Product.findById(id).populate("creator").populate("votes");
+const deleteRecord = id => Product.deleteOne({ _id: id });
 
 const voteUp = async (productId, userId) => {
   let product = await Product.findById(productId);
@@ -27,13 +33,43 @@ const voteDown = async (productId, userId) => {
   return;
 };
 
+const favorite = async (productId, userId) => {
+  let product = await Product.findById(productId);
+  let user = await User.findById(userId);
+
+  let favoriteProducts = user.favorites;
+  favoriteProducts.push(product);
+
+  await User.updateOne({ _id: userId }, { favorites: favoriteProducts });
+};
+
+const edit = (productId, brand, model, description, imgUrl, condition, year, price, productType) =>
+  Product.updateOne(
+    { _id: productId },
+    {
+      brand,
+      model,
+      description,
+      imgUrl,
+      condition,
+      year,
+      price,
+      productType
+    },
+    { runValidators: true }
+  );
 
 const productServices = {
   create,
   getAll,
   getOne,
   deleteRecord,
+  getTop10,
   voteUp,
-  voteDown
+  favorite,
+  voteDown,
+  edit,
+  comment,
+  getLatest
 };
 module.exports = productServices;
