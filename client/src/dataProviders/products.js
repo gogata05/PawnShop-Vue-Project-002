c79bab873f275cc1e30bc7125abdb933eb68c2ec
Georgi Markov <gogata1905@gmail.com>
2024-10-23 12:16:24 +0300

    add crud product operations

---
+++ client/src/dataProviders/products.js
@@ -0,0 +1,51 @@
+// client\src\dataProviders\products.js
+import axiosInstance from "../configs/axios";
+
+export async function getProducts(type) {
+  try {
+    const res = await axiosInstance.get(`/${type}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+export async function getProduct(id) {
+  try {
+    const res = await axiosInstance.get(`/details/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function addProduct(productData) {
+  try {
+    const res = await axiosInstance.post("/add", productData);
+    return res.data;
+  } catch (error) {
+    let backendError = error.response.data.error;
+    return { error: backendError };
+  }
+}
+
+export async function editProduct(productData, id) {
+  try {
+    const res = await axiosInstance.post(`/edit/${id}`, productData);
+    return res.data;
+  } catch (error) {
+    let backendError = error.response.data.error;
+    return { error: backendError };
+  }
+}
+
+export async function deleteProduct(id) {
+  try {
+    const res = await axiosInstance.get(`/delete/${id}`);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
